0f997841c83974e86920a85d140ea8033e2047a0
Nabilaaputri <113659371+Nabilaaputri@users.noreply.github.com>
2022-09-16 11:57:50 +0700

    Add files via upload

---
+++ pelatihan.py
@@ -0,0 +1,13 @@
+#bangun ruang balok
+print("PROGRAM MENGHITUNG VOLUME BALOK")
+p = int(input("memasukkan panjangnya ="))
+l = int(input("memasukkan lebar ="))
+t = int(input("memasukkan tinggi ="))
+volume = p*l*t
+print("volume baloknya adalah = ", volume)
+
+#bangun ruang kubus
+print("PROGRAM MENGHITUNG VOLUME KUBUS")
+s = int(input("memasukkan rusuknya ="))
+volume = s*s*s
+print("volume kubusnya adalah = ", volume)
